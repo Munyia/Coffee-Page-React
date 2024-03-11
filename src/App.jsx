2fb2084da369b146f36e0258367435b1422735ca
Vivian <ndidiranjo@gmail.com>
2024-03-11 15:46:09 +0100

    adding react

---
+++ src/App.jsx
@@ -0,0 +1,303 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <body>
+    <header>
+      <div id="header1">
+        <img id="img1" src="res/Logo.svg" alt="" />
+        <p id="headleft">
+          <img id="sea" src="res/pngaaa.com-25432.png" alt="" /> Search
+          something here....
+        </p>
+        <p id="headright">
+          <button id="but1">search</button>
+          <img src="res/Basket.svg" alt="" />
+          <img src="res/Button.svg" alt="" />
+          <img src="res/Like.svg" alt="" />
+        </p>
+      </div>
+      <div id="straightline"></div>
+      <nav>
+        <a href="Menu">Menu</a>
+        <a href="About">About</a>
+        <a href="Contact" style={{display:'inline-block'}}>
+          Contact
+          <img src="res/chevron/down.png" alt="" style={{verticalAlign: "middle;"}} />
+        </a>
+      </nav>
+      <section id="sec1">
+        <div className="quote">
+          <h1>SAVOR THE MOMENT <br/> WITH EVERY SIP</h1>
+          <span className="quote-top">“</span>
+          <span className="quote-bottom">”</span>
+        </div>
+        <div id="div2"> 
+            <p id="p1"> Discover our carefully crafted coffee blends, made with love and passion</p>
+            <p id="but">
+            <button id="ab">About</button>
+            <button id="or">Order Now</button>
+        </p>
+        </div>
+      </section>
+    </header>
+      <section id="sec2">
+        <div id="sec2a">
+          <div>
+            <p id="sec22a">WHETHER YOU'RE LOOKING FOR A classNameIC ESPRESSO <br/>OR A SPECIALTY LATTE, WE HAVE SOMETHING FOR <br/>EVERY COFFEE LOVER AT BREW TIME.</p>
+          </div>
+          <div>
+            <img id="cof" src="res/png4 1.svg" alt="" />
+          </div>
+          <div className="beans">
+            <p id="sec2-2"> OUR CAREFULLY SELECTED BEANS ARE ROASTED TO <br/>PERFECTION, ENSURING A RICH AND FULL-BODIED <br/>FLAVOR IN EVERY SIP.</p>
+          </div>
+          <img src="res/png00.png" className="background-image1" alt="Background Image"/>
+          <img src="res/png2 2.svg" className="background-image2" alt="Background Image"/>
+</div>
+        <div id="sec3" >
+          <div className="line"></div>
+          <p id="line1">EXCLUSIVE COFFEE</p>
+          <div className="line"></div>
+        </div>
+        </section>
+      <section id="boddy">
+          <div id="sec3-1">
+            <div id="box1">
+              <img src="res/taisiia-shestopal-xO9NotFY4mU-unsplash 1.svg" alt=""/>
+              <p className="amer">Ameriacno </p>
+              <p className="box11">Lorem ipsum dolor sit amet,<br/> margin and padding.</p>
+              <p className="box12">$19.00</p>
+            </div>
+              <div id="box2">
+                <img src="res/melissa-walker-horn-cGxX3OLFg5M-unsplash 1.svg" alt=""/>
+                <p className="amer">Ameriacno </p>
+                <p className="box11">Lorem ipsum dolor sit amet,<br/> margin and padding.</p>
+                <p className="box12">$20.00</p>
+              </div>
+              <div id="box3">
+                <img src="res/taisiia-shestopal-xO9NotFY4mU-unsplash 1.svg" alt=""/>
+                <p className="amer">Ameriacno </p>
+                <p className="box11">Lorem ipsum dolor sit amet,<br/> margin and padding.</p>
+                <p className="box12">$15.00</p>
+              </div>
+            </div>
+          </section>
+          <section id="sec4">
+            <div className="sec4adiv">
+              <p id="sec4a">Menu</p>
+            </div>
+            <div id="sec4b">
+              <div id="sec4bb">
+                <div id="sec4c">
+                  <div className="sec4d">
+                    <p className="num">500+</p>
+                    <p className="alph">Over 500 cups <br/> brewed daily</p>
+                  </div>
+                <div  className="sec4d">
+                  <p className="num">1000+</p>
+                  <p className="alph">Join our community <br/>of 1,000+ daily <br/>customers</p>
+                </div>
+                <div  className="sec4d">
+                  <p className="num">5</p>
+                  <p className="alph">Visit one of our 5 <br/> convenient locations <br/>across the city</p>
+                </div>
+              </div>
+                <div id="sec4e">
+                  <p>Join our community of coffee lovers and enjoy over 500 cups <br/> brewed daily by our expert baristas. With 5 convenient <br/> locations across the city, it's easy to savor our rich, full- <br/> bodied blends and friendly service. Trust our <span>passionate team <br/> </span> to deliver the <span>perfect cup </span> every time.</p>
+                </div>
+              </div>
+              <div id="sec4f">
+                <img src="res/pngwing 2.svg" alt=""/>
+              </div>
+            </div>
+          </section>
+          <section>
+            <hr className="cof-line"/>
+            <div id="sec5">
+              <img src="res/Hilton_Worldwide_logo 1.png" alt=""/>
+              <img src="res/Frame 48095424.png" alt=""/>
+              <img src="res/Group 48095551.svg" alt=""/>
+              <img src="res/Group 48095552.png" alt=""/>
+              <img src="res/Group 48095553.png" alt=""/>
+            </div>
+            <hr className="cof-line"/>
+          </section>
+          <section id="sec6">
+            <div id="sec6a">
+              <div id="hea">
+                <p id="fre">FREQUENTLY ASKED QUESTIONS</p>
+                <p id="lo">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem <br/>Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+              </div>
+            </div>
+            <div id="sec6b">
+              <div className="sec6c">
+                <div className="sec6ca">
+                  <img id="img11" src="res/Frame.png" alt=""/>
+                  <p className="many">OUR COFFEE</p>
+                </div>
+                <div>
+                  <p>Lorem Ipsum is simply dummy <br/>text of the printing and <br/> typesetting industry. Lorem Ipsum <br/> has been the </p>
+                </div>
+                <div>
+                  <p className="see">See all </p>
+                </div>
+              </div>
+              <div className="vl"></div>
+              <div className="sec6c">
+                <div className="sec6ca">
+                  <img src="res/Frame (1).svg" alt=""/>
+                  <p className="many">OUR BEANS</p>
+                </div>
+                <div>
+                  <p>Lorem Ipsum is simply dummy <br/>text of the printing and <br/> typesetting industry. Lorem Ipsum <br/> has been the </p>
+                </div>
+                <div>
+                  <p className="see">See all </p>
+                </div>
+              </div>
+              <div className="vl"></div>
+              <div className="sec6c">
+                <div className="sec6ca">
+                  <img src="res/Frame (2).svg" alt=""/>
+                  <p className="many">PASTRIES</p>
+                </div>
+                <div>
+                  <p>Lorem Ipsum is simply dummy <br/>text of the printing and <br/> typesetting industry. Lorem Ipsum <br/> has been the </p>
+                </div>
+                <div>
+                  <p className="see">See all </p>
+                </div>
+              </div>
+            <div className="vl"></div>
+              <div className="sec6c">
+                <div className="sec6ca">
+                  <img src="res/Frame (3).svg" alt=""/>
+                  <p className="many">PRE-ORDER</p>
+                </div>
+                <div>
+                  <p>Lorem Ipsum is simply dummy <br/>text of the printing and <br/> typesetting industry. Lorem Ipsum <br/> has been the </p>
+                </div>
+                <div>
+                  <p className="see">See all </p>
+                </div>
+              </div>
+            </div>
+            <div id="sec6d">
+              <p id="sec6da">About</p>
+              <p id="sec6db">Contact</p>
+            </div>
+          </section>
+          <section id="sec7">
+            <div id="gue">
+              <div className="lines"></div>
+              <p id="lines1">OUR GUESTBOOK</p>
+              <div className="lines"></div>
+            </div>
+            <div id="sec7a">
+              <div id="sec7b">
+                <img src="res/Frame (5).svg" alt=""/>
+                <p>Lorem ipsum dolor sit amet, ssfsf consectetur <br/> adipiscing elit. Nunc vulputate libero et velit <br/> interdum, ac aliquet odio mattis.</p>
+                 <div className="sec7f">
+                   <img className="sec7e" src="res/Ellipse 10 (1).svg" alt=""/>
+                     <p>Kim Lee Jon</p>
+                     <p>Artist</p>
+                  </div>
+              </div>
+            <div id="sec7c">
+              <img src="res/Frame (6).svg" alt=""/>
+              <p>Lorem ipsum dolor sit amet, ssfsf consectetur <br/> adipiscing elit. Nunc vulputate libero et velit <br/> interdum, ac aliquet odio mattis.</p>
+              <div className="sec7f">
+                <img className="sec7e" src="res/Ellipse 10 (2).svg" alt=""/>
+                  <p>DareL McLovin</p>
+                  <p>Driver</p>
+               </div>
+            </div>
+            <div id="sec7d">
+              <img src="res/Frame (5).svg" alt=""/>
+              <p>Lorem ipsum dolor sit amet, ssfsf consectetur <br/> adipiscing elit. Nunc vulputate libero et velit <br/> interdum, ac aliquet odio mattis.</p>
+              <div className="sec7f">
+                <img className="sec7e" src="res/Ellipse 10 (3).svg" alt=""/>
+                  <p>Alexa Blair</p>
+                  <p>Dentist</p>
+                </div>
+               </div>
+          </div>
+          </section>
+          <section id="sec8">
+            <img src="res/Map.svg" alt=""/>
+          </section>
+          <section>
+            <div id="sec9">
+              <div id="sec9a">
+                <p id="get">GET IN TOUCH WITH US</p>
+              <p id="lo">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem <br/> Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+            </div>
+            <div id="sec9b">
+              <div id="sec9c">
+                <div className="buttt">Full Name</div>
+                <div className="buttt">Email</div>
+              </div>
+              <div id="sec9d">
+                <div id="sec9da">Message</div>
+              </div>
+              <div id="sec9e">
+                <div id="sec9ea">Submit</div>
+              </div>
+              <div id="sec9f">
+                <img id="sec9fa" src="res/Group 77725498.svg" alt=""/>
+                <img id="sec9fb" src="res/Group 33.svg" alt=""/>
+              </div>
+              </div>
+            </div>
+          </section>
+          <footer>
+            <div id="sec10">
+                <div>
+                  <img src="res/Logo section (1).svg" alt=""/>
+                </div>
+               <div id="sec10b">
+                <div>
+                  <p id="sec10c">PRODUCTS</p>
+                  <p>Features</p>
+                  <p>Solutions</p>
+                  <p>Integrations</p>
+                  <p>Enterprise</p>
+                  <p>Solutions</p>
+                </div>
+                <div>
+                  <p id="sec10c">RESOURCES</p>
+                  <p>Partners</p>
+                  <p>Community</p>
+                  <p>Developers</p>
+                  <p>App</p>
+                  <p>Blog</p>
+                </div>
+                <div>
+                  <p id="sec10c">WHY CHOOSE US</p>
+                  <p>Channels</p>
+                  <p>Scale</p>
+                  <p>Watch the Demo</p>
+                  <p>Our competition</p>
+                </div>
+                <div>
+                  <p id="sec10c">Company</p>
+                  <p>About us</p>
+                  <p>News</p>
+                  <p>Leadership</p>
+                </div>
+               </div>
+          </div>
+          <div id="copy">
+            <p>&copy; 2023 Brew Time. All Rights Reserved.</p>
+          </div>
+          </footer>
+  </body>
+    </>
+  )
+}
+
+export default App
